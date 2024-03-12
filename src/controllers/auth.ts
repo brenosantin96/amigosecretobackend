525d5e58f70b339a94726775130366f429e5c638
brenosantin96 <brenolimaa96@gmail.com>
2024-03-12 22:28:10 +0100

    teste

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
 import { z } from 'zod'
+import * as auth from "../services/auth";
 
 
 export const login: RequestHandler = (req, res) => {
@@ -9,10 +10,12 @@ export const login: RequestHandler = (req, res) => {
 
     const body = loginSchema.safeParse(req.body);
 
-    if(!body.success) return res.json({error: 'Dados inválidos'})
+    if (!body.success) return res.json({ error: 'Dados inválidos' })
 
-    //validate password and generate token
-    body.data.password
-    //return requisition
+    if (!auth.validatePassword(body.data.password)) {
+        return res.status(403).json({ error: 'Acesso negado' })
+    }
+
+    return res.json({ token: auth.createToken() });
 
 }
